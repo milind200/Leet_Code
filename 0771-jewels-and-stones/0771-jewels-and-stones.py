b3157094d6f0c097e5877df231d1c6b9ade6bfc6
milind200 <62588266+milind200@users.noreply.github.com>
2023-08-22 16:48:20 +0530

    Time: 46 ms (41.06%), Space: 16.2 MB (91.07%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,11 +1,9 @@
-int numJewelsInStones(char * jewels, char * stones){
-   int num=0;
-   for(int i=0;i<strlen(stones);i++){
-       for(int j=0;j<strlen(jewels);j++){
-           if(stones[i]==jewels[j]){
-               num++;
-           }
-       }
-   }
-return num;
-}
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        no=0; 
+        jewels=sorted(jewels)
+        for i in jewels:
+            if i in stones:
+                no+=stones.count(i)
+        return no
+        
