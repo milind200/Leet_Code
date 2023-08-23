156156bd74a58517d7c372e47d7632c5c875f440
milind200 <62588266+milind200@users.noreply.github.com>
2023-08-23 17:11:26 +0530

    Time: 1 ms (65.90%), Space: 40.5 MB (80.06%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,17 +1,6 @@
-char * interpret(char * command){
-    int index=0;
-    char* p=(char*)malloc((strlen(command)+1)*sizeof(char));
-    for(int i=0;command[i] !='\0';i++){
-        if(command[i]=='('&& command[i+1]==')'){
-            p[index++]='o';i++;
-        }else if(command[i]=='('){
-            p[index++]='a';i++;
-            p[index++]='l';i++;
-            i++;
-        }else{
-            p[index++]=command[i];
-        }
-    }p[index]='\0';
-    
-    return p;
+class Solution {
+    public String interpret(String command) {
+        String str= command.replace("()","o");
+        return str.replace("(al)","al");
     }
+}
