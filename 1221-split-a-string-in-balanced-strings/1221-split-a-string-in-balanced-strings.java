f4cceb3827996e962a34808d3281839e8a94519c
milind200 <62588266+milind200@users.noreply.github.com>
2023-08-28 15:27:07 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (43.76%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,22 +1,19 @@
 class Solution {
-public:
-    int balancedStringSplit(string s) {
-         int count=0,lcount=0,rcount=0,i=0;
-    while(i<s.length()){
-        if(s[i]=='L'){
-            lcount++;
+    public int balancedStringSplit(String s) {
+        int count=0,lcount=0,rcount=0,i=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='L'){
+                lcount++;
+            }else{
+                rcount++;
+            }
+            if(lcount==rcount){
+                count++;
+                lcount=0;
+                rcount=0;
+            }
+            i++;
         }
-        if(s[i]=='R'){
-            rcount++;
-        }
-        if(lcount==rcount){
-            count++;
-            lcount=0;
-            rcount=0;
-        }
-        i++;
+        return count;
     }
-    return count;
-        
-    }
-};
+}
