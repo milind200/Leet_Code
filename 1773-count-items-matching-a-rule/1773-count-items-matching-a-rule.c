035dda0d39e7b8b19a3b375cf52e327adb323357
milind200 <62588266+milind200@users.noreply.github.com>
2023-08-29 15:25:41 +0530

    Time: 49 ms (98.29%), Space: 13.6 MB (88.89%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.c
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.c
@@ -3,12 +3,14 @@ int countMatches(char *** items, int itemsSize, int* itemsColSize, char * ruleKe
     if(strcmp(ruleKey,"type")==0){
         i=0;
     }
-    if(strcmp(ruleKey,"color")==0){
+    else if(strcmp(ruleKey,"color")==0){
         i=1;
     }
-    if(strcmp(ruleKey,"name")==0){
+    else
+    {
         i=2;
     }
+    
     printf("%d",i);
       for(int j=0;j<itemsSize;j++){
           if (strcmp(ruleValue,items[j][i])==0 ){
