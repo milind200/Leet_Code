da45e5d44cfaf99722dddeb980db67e45e22ac5c
milind200 <62588266+milind200@users.noreply.github.com>
2023-09-12 13:18:06 +0530

    Time: 187 ms (79.63%), Space: 18.4 MB (6.70%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        
+         return[first]+[first:=first^x for x in encoded];
+        
+        
+        
+        
