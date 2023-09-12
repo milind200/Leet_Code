74732ae1b88f13ec1c4c232de0527c76479f6c5f
milind200 <62588266+milind200@users.noreply.github.com>
2023-09-12 12:50:29 +0530

    Time: 21 ms (73.86%), Space: 26.2 MB (16.76%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -4,7 +4,7 @@ public:
       vector<int>arr;
         arr.push_back(first);
         for(int i=0;i<encoded.size();i++){
-            arr.push_back(arr.at(i)^encoded.at(i));
+            arr.push_back(arr[i]^encoded[i]);
         }
         return arr;
     }
