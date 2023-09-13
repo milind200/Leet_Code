7812d62b316e1642ee6e096684efe35006da5af8
milind200 <62588266+milind200@users.noreply.github.com>
2023-09-13 13:51:18 +0530

    Time: 1 ms (89.79%), Space: 43.6 MB (80.99%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        List<Integer> arr=new LinkedList<>();
-        for(int i=0;i<nums.length;i+=2){
-            for(int j=0;j<nums[i];j++){
-                arr.add(nums[i+1]);
-            }
-                
-        }
-        return arr.stream().mapToInt(Integer::intValue).toArray();
+        int size=0,p=0;
+        for(int i=0;i<nums.length;i+=2) size+=nums[i];
+        int []arr=new int[size];
+        for(int i=0;i<nums.length;i+=2)
+            for(int j=0;j<nums[i];j++) arr[p++]=nums[i+1];
+        return arr;
     }
 }
