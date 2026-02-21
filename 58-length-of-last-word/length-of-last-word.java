7f382af8cd0ff1f6c05620f6e5d8f3e53f01ae54
milind200 <62588266+milind200@users.noreply.github.com>
2026-02-21 18:22:58 +0530

    Time: 2 ms (10.49%) | Memory: 43.2 MB (32.78%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,9 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
       s=s.trim();
-     // System.out.print(s.trim());
-     String[] words= s.split("\\s+");
-
+      String[] words= s.split("\\s+");
       return words[words.length-1].length();
     }
 }
